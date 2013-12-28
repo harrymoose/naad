8fe59045c396cca3090b720cc277b3a53c11e342
harrymoose <harinderbalala@gmail.com>
2013-12-28 16:47:42 +0530

    Initial commit

---
+++ jukebox.js
@@ -0,0 +1,21 @@
+
+window.onload= function(){
+	
+/*var manjh = document.getElementById('drum');
+	manjh.src="sounds/Clap.wav";
+	manjh.play();
+*/
+var snare = document.getElementById('snare');
+snare.src="sounds/snare.wav";
+var clap = document.getElementById('clap');
+clap.src="sounds/Clap.wav";
+}
+
+function snare1()
+{
+snare.play();
+}
+function clapfunc()
+{clap.play();
+}
+
